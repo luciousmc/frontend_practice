44ffa4409fca001fe8fcf8cded454036656574b4
thebearingedge <contact@timdav.is>
2019-04-30 08:37:26 -0700

    Add Array.prototype.filter exercise. #17

---
+++ exercises/array-filter/main.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console, no-unused-vars */
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+var names = [
+  'Bill',
+  'Monique',
+  'Dan',
+  'TJ',
+  'Scott',
+  'Tim H.',
+  'Cody',
+  'Brett',
+  'Andy',
+  'Tim D.'
+];
