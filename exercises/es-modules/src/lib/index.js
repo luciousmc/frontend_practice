53d4da79c205cc816a9652a1c7f813e2f6df4753
thebearingedge <contact@timdav.is>
2019-05-05 16:44:02 -0700

    Add ES Modules exercise. #60

---
+++ exercises/es-modules/src/lib/index.js
@@ -0,0 +1,7 @@
+const noop = require('./noop');
+const toArray = require('./to-array');
+const createElement = require('./create-element');
+
+exports.noop = noop;
+exports.toArray = toArray;
+exports.createElement = createElement;
