f3f3b1169bdc5153b90184e2de28ab55c0191191
NoelCarino <leononirac1992@gmail.com>
2019-09-26 18:08:31 +0000

    Array-Filter | Noel Carino

--- exercises/array-filter/main.js
+++ exercises/array-filter/main.js
@@ -14,3 +14,15 @@ var names = [
   'Andy',
   'Tim D.'
 ];
+
+var evenNumbers = numbers.filter(numbers => numbers % 2 === 0);
+console.log('Even Numbers - ', evenNumbers);
+
+var overFive = numbers.filter(numbers => numbers > 5);
+console.log('Numbers > 5 - ', overFive);
+
+var startWithT = names.filter(names => names.startsWith('T'));
+console.log('Starts with T (startsWith method) - ', startWithT);
+
+var haveD = names.filter(names => names.includes('d') || names.includes('D'));
+console.log('Includes letter D - ', haveD);
