b8d404b25c4ae578fb39491c43267c5491fe9dfc
Marlon <luciousmc@icloud.com>
2020-01-08 11:29:00 -0800

    create an array of 3 books

--- exercises/javascript-and-json/main.js
+++ exercises/javascript-and-json/main.js
@@ -1,1 +1,20 @@
 /* eslint-disable no-console */
+const books = [
+  {
+    title: null,
+    isbn: null,
+    author: null
+  },
+  {
+    title: null,
+    isbn: null,
+    author: null
+  },
+  {
+    title: null,
+    isbn: null,
+    author: null
+  }
+];
+const output = JSON.stringify(books);
+console.log('the output is: ', output);
