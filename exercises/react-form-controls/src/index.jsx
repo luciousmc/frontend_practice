fdf21daf878a1e0a10601a4fa563a0aed5f7806d
Marlon <luciousmc@icloud.com>
2020-01-13 20:22:46 -0800

    package.json and webpack configured html skeleton

--- exercises/react-form-controls/src/index.jsx
+++ exercises/react-form-controls/src/index.jsx
@@ -1,1 +1,26 @@
 /* eslint-disable no-console */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: null
+    };
+  }
+  render() {
+    return (
+      <form>
+        <label htmlFor="email">Email Address</label>
+        <input type="text" name="email" />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.getElementById('root')
+);
