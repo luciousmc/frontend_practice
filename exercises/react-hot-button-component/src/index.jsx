6a084a85a780372486c129b717de0b72912183e1
Marlon <luciousmc@icloud.com>
2020-01-14 17:25:16 -0800

    uses array instead of object to choose bgColor

--- exercises/react-hot-button-component/src/index.jsx
+++ exercises/react-hot-button-component/src/index.jsx
@@ -5,19 +5,37 @@ class HotButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      '3': 'purple',
-      '6': 'violet',
-      '9': 'red',
-      '12': 'orange',
-      '15': 'white'
+      clicks: 0,
+      colors: ['white', 'purple', 'violet', 'red', 'orange', 'yellow']
     };
-    this.clicks = 0;
+    this.handleClick = this.handleClick.bind(this);
+  }
+  renderBgColor() {
+    let value = Math.floor(this.state.clicks / 3);
+    // eslint-disable-next-line no-console
+    console.log('value is: ', value, 'clicks is: ', this.state.clicks);
+    if (value === 6) {
+      return this.state.colors[0];
+    }
+    return this.state.colors[value];
+  }
+  handleClick(event) {
+    let value = this.state.clicks / 3;
+    if (value === 6) {
+      this.setState(state => ({
+        clicks: 0
+      }));
+    }
+    this.setState(state => ({
+      clicks: state.clicks + 1
+    }));
   }
   render() {
+    const bgClass = this.renderBgColor();
     return (
-      <div id="hot-button">
-        <p>Clicks: {this.clicks}</p>
-        <p>Color: {this.state[toString(this.clicks)]}</p>
+      <div id="hot-button" onClick={this.handleClick} className={bgClass}>
+        <p>Clicks: {this.state.clicks}</p>
+        <p>Color: {bgClass}</p>
       </div>
     );
   }
