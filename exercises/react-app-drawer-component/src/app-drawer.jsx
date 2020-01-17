20cdeb6bb4dee7deede9b4f512643ee1f798a331
Marlon <luciousmc@icloud.com>
2020-01-16 20:57:58 -0800

    html skeleton

---
+++ exercises/react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AppDrawer extends React.Component {
+  render() {
+    return (
+      <div className="drawer-container">
+        <div className="drawer-menu-container"></div>
+      </div>
+    );
+  }
+}
+
+export default AppDrawer;
--- exercises/react-app-drawer-component/webpack.config.js
+++ exercises/react-app-drawer-component/webpack.config.js
@@ -2,17 +2,19 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  rules: [
-    {
-      test: /\.jsx?$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          plugins: [
-            '@babel/plugin-transform-react-jsx'
-          ]
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
         }
       }
-    }
-  ]
+    ]
+  }
 };
