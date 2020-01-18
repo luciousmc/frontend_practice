569d2f1a4459f3d0b39a2c7820d0a68c215f176d
Marlon <luciousmc@icloud.com>
2020-01-17 20:41:42 -0800

    style menu imports font awesome for icons

--- exercises/react-app-drawer-component/src/app-drawer.jsx
+++ exercises/react-app-drawer-component/src/app-drawer.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: false
+    };
+  }
+  handleMenuClick(event) {}
   render() {
     return (
       <div className="drawer-container">
-        <div className="drawer-menu-container"></div>
+        <div className="drawer-content-container">
+          <h1 className="title">Menu</h1>
+          <hr className="separator"/>
+          <div className="menu">
+            <ul className="nav">
+              <li>Home</li>
+              <li>About</li>
+              <li>Links</li>
+              <li>Contact</li>
+              <li>Forum</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
