2982119dfd8256ca42369249b90d361173adaab0
Marlon <luciousmc@icloud.com>
2020-01-26 18:36:31 -0800

    basic skeleton file structure made

---
+++ exercises/react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: null
+    };
+  }
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default ValidatedInput;
