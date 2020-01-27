52aed2c20deced0234233a95427aba1ed268f235
Marlon <luciousmc@icloud.com>
2020-01-26 19:44:04 -0800

    handlechange constructed added Font Awesome

--- exercises/react-validated-input-component/src/validated-input.jsx
+++ exercises/react-validated-input-component/src/validated-input.jsx
@@ -4,14 +4,28 @@ class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: null
+      input: ''
     };
+    this.handlePasschange = this.handlePasschange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handlePasschange(event) {
+    this.setState({ input: event.target.value });
+    // eslint-disable-next-line no-console
+    console.log('the state is: ', this.state);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    // eslint-disable-next-line no-console
+    console.log('the event object is: ', event);
+
   }
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor="password">Password: </label>
-        <input type="text" name="password" />
+        <input type="password" name="password" onChange={this.handlePasschange} value={this.state.input} />
+        <input type="submit" />
       </form>
     );
   }
